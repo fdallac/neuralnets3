f59b89dae37039756b063b78b298c847bb6aa02d
fdallac <filippo.dallac@live.it>
2025-11-23 23:25:57 +0100

    Implemented basic matrix operators

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,4 +1,5 @@
 #include "matrix.hpp"
+#include "matmul.hpp"
 
 template<typename T>
 Matrix<T>::Matrix()
@@ -26,6 +27,7 @@ const T& Matrix<T>::operator()(std::size_t i, std::size_t j) const {
     return data_[i * cols_ + j];
 } 
 
+
 template<typename T>
 T& Matrix<T>::at(std::size_t i, std::size_t j) {
     if (i >= rows_ || j >= cols_) {
@@ -42,6 +44,57 @@ const T& Matrix<T>::at(std::size_t i, std::size_t j) const {
     return data_[i * cols_ + j];
 }
 
+template<typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T>& other) const {
+    return MatMul<T>::mm(*this, other);
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator+(const Matrix<T>& other) const {
+    if (rows_ != other.rows() || cols_ != other.cols()) {
+        throw std::invalid_argument("Matrix dimensions does not match for addition");
+    }
+    Matrix<T> result(rows_, cols_);
+    for (std::size_t i = 0; i < rows_; ++i) {
+        for (std::size_t j = 0; j < cols_; ++j) {
+            result(i, j) = (*this)(i, j) + other(i, j);
+        }
+    }
+    return result;
+}
+
+template<typename T>
+Matrix<T>& Matrix<T>::operator+=(const Matrix<T>& other) {
+    if (rows_ != other.rows() || cols_ != other.cols()) {
+        throw std::invalid_argument("Matrix dimensions does not match for addition");
+    }
+    for (std::size_t i = 0; i < rows_; ++i) {
+        for (std::size_t j = 0; j < cols_; ++j) {
+            (*this)(i, j) += other(i, j);
+        }
+    }
+    return *this;
+}
+
+template<typename T>
+bool Matrix<T>::operator==(const Matrix<T>& other) const {
+    if (rows_ != other.rows() || cols_ != other.cols()) {
+        return false;
+    }
+    for (std::size_t i = 0; i < rows_; ++i) {
+        for (std::size_t j = 0; j < cols_; ++j) {
+            if ((*this)(i, j) != other(i, j)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+template<typename T>
+bool Matrix<T>::operator!=(const Matrix<T>& other) const {
+    return !(*this == other);
+}
 
 // Explicit template instantiation for common types
 template class Matrix<int>;
