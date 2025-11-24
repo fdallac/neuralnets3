b0adadf76a5da7ac8b2bc16e77181b19350e1104
fdallac <filippo.dallac@live.it>
2025-11-24 23:43:27 +0100

    Developed loop unrolling method

--- include/matmul.hpp
+++ include/matmul.hpp
@@ -4,7 +4,9 @@
 
 enum class MatMulMethod {
     Vanilla,
-    LoopUnrolling,
+    LoopUnrolled4,
+    LoopUnrolled8,
+    LoopUnrolled16,
     SIMD,
     OpenMP,
     CUDA
@@ -18,7 +20,9 @@ public:
 
     // Specific implementations
     static Matrix<T> mm_vanilla(const Matrix<T>& A, const Matrix<T>& B);
-    // static Matrix<T> mm_loop_unrolling(const Matrix<T>& A, const Matrix<T>& B);
+    static Matrix<T> mm_unrolled4(const Matrix<T>& A, const Matrix<T>& B);
+    // static Matrix<T> mm_unrolled8(const Matrix<T>& A, const Matrix<T>& B);
+    // static Matrix<T> mm_unrolled16(const Matrix<T>& A, const Matrix<T>& B);
     // static Matrix<T> mm_simd(const Matrix<T>& A, const Matrix<T>& B);
     // static Matrix<T> mm_openmp(const Matrix<T>& A, const Matrix<T>& B);
     // static Matrix<T> mm_cuda(const Matrix<T>& A, const Matrix<T>& B);
--- src/matmul.cpp
+++ src/matmul.cpp
@@ -5,6 +5,9 @@ Matrix<T> MatMul<T>::mm(const Matrix<T>& A, const Matrix<T>& B, MatMulMethod met
     switch (method) {
         case MatMulMethod::Vanilla:
             return mm_vanilla(A, B);
+        case MatMulMethod::LoopUnrolled4:
+            return mm_unrolled4(A, B);
+        
         // Future methods can be added here
         default:
             throw std::invalid_argument("Unknown matrix multiplication method");
@@ -31,6 +34,38 @@ Matrix<T> MatMul<T>::mm_vanilla(const Matrix<T>& A, const Matrix<T>& B) {
 }
 
 
+template<typename T>
+Matrix<T> MatMul<T>::mm_unrolled4(const Matrix<T>& A, const Matrix<T>& B) {
+    if (A.cols() != B.rows()) {
+        throw std::invalid_argument("Incompatible matrix dimensions for multiplication");
+    }
+    Matrix<T> C(A.rows(), B.cols());
+    const std::size_t N = A.cols();
+    for (std::size_t i = 0; i < A.rows(); ++i) {
+        for (std::size_t j = 0; j < B.cols(); ++j) {
+            T acc0 = T{};
+            T acc1 = T{};
+            T acc2 = T{};
+            T acc3 = T{};
+            std::size_t k = 0;
+            // Process 4 elements at a time
+            for (; k + 3 < N; k += 4) {
+                acc0 += A(i, k    ) * B(k,     j);
+                acc1 += A(i, k + 1) * B(k + 1, j);
+                acc2 += A(i, k + 2) * B(k + 2, j);
+                acc3 += A(i, k + 3) * B(k + 3, j);
+            }
+            // Add the partial sums together
+            T sum = acc0 + acc1 + acc2 + acc3;
+            // Handle leftover values
+            for (; k < N; ++k) {
+                sum += A(i, k) * B(k, j);
+            }
+            C(i, j) = sum;
+        }
+    }
+    return C;
+}
 
 
 
