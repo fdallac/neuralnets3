22bc4b078dc539c6a75675abd3f3f575984603e1
fdallac <filippo.dallac@live.it>
2025-12-08 12:00:18 +0100

    Refactored Activation functions implementation

--- include/activation.hpp
+++ include/activation.hpp
@@ -2,74 +2,88 @@
 #include "matrix.hpp"
 #include <cmath>
 
+template<typename T>
+class Activation {
+    public:
+        virtual T forward(const Matrix<T>& M) = 0;
+        virtual Matrix<T> backward(const Matrix<T>& M) = 0;
+        virtual ~Activation() = default;
+};
 
 
 template<typename T>
-Matrix<T> relu(const Matrix<T>& M) {
-    Matrix<T> result(M.rows(), M.cols());
-    for (std::size_t i = 0; i < M.rows(); ++i) {
-        for (std::size_t j = 0; j < M.cols(); ++j) {
-            result(i, j) = M(i, j) > T{} ? M(i, j) : T{};
+class ReLU : public Activation<T> {
+    public:
+        static Matrix<T> forward(const Matrix<T>& M) {
+            Matrix<T> result(M.rows(), M.cols());
+            for (std::size_t i = 0; i < M.rows(); ++i) {
+                for (std::size_t j = 0; j < M.cols(); ++j) {
+                    result(i, j) = M(i, j) > T{} ? M(i, j) : T{};
+                }
+            }
+            return result;
         }
-    }
-    return result;
-}
 
-template<typename T>
-Matrix<T> drelu(const Matrix<T>& M) {
-    Matrix<T> result(M.rows(), M.cols());
-    for (std::size_t i = 0; i < M.rows(); ++i) {
-        for (std::size_t j = 0; j < M.cols(); ++j) {
-            result(i, j) = M(i, j) > T{} ? T{1} : T{};
+        static Matrix<T> backward(const Matrix<T>& M) {
+            Matrix<T> result(M.rows(), M.cols());
+            for (std::size_t i = 0; i < M.rows(); ++i) {
+                for (std::size_t j = 0; j < M.cols(); ++j) {
+                    result(i, j) = M(i, j) > T{} ? T{1} : T{};
+                }
+            }
+            return result;
         }
-    }
-    return result;
-}
-
-
-template<typename T>
-Matrix<T> sigmoid(const Matrix<T>& M) {
-    Matrix<T> result(M.rows(), M.cols());
-    for (std::size_t i = 0; i < M.rows(); ++i) {
-        for (std::size_t j = 0; j < M.cols(); ++j) {
-            result(i, j) = T{1} / (T{1} + std::exp(-M(i, j)));
-        }
-    }
-    return result;
-}
-
-template<typename T>
-Matrix<T> dsigmoid(const Matrix<T>& M) {
-    Matrix<T> result(M.rows(), M.cols());
-    for (std::size_t i = 0; i < M.rows(); ++i) {
-        for (std::size_t j = 0; j < M.cols(); ++j) {
-            T sig = T{1} / (T{1} + std::exp(-M(i, j)));
-            result(i, j) = sig * (T{1} - sig);
-        }
-    }
-    return result;
-}
+};
 
 
 template<typename T>
-Matrix<T> tanh(const Matrix<T>& M) {
-    Matrix<T> result(M.rows(), M.cols());
-    for (std::size_t i = 0; i < M.rows(); ++i) {
-        for (std::size_t j = 0; j < M.cols(); ++j) {
-            result(i, j) = std::tanh(M(i, j));
+class Sigmoid : public Activation<T> {
+    public:
+        static Matrix<T> forward(const Matrix<T>& M) {
+            Matrix<T> result(M.rows(), M.cols());
+            for (std::size_t i = 0; i < M.rows(); ++i) {
+                for (std::size_t j = 0; j < M.cols(); ++j) {
+                    result(i, j) = T{1} / (T{1} + std::exp(-M(i, j)));
+                }
+            }
+            return result;
         }
-    }
-    return result;
-}
+
+        static Matrix<T> backward(const Matrix<T>& M) {
+            Matrix<T> result(M.rows(), M.cols());
+            for (std::size_t i = 0; i < M.rows(); ++i) {
+                for (std::size_t j = 0; j < M.cols(); ++j) {
+                    T sig = T{1} / (T{1} + std::exp(-M(i, j)));
+                    result(i, j) = sig * (T{1} - sig);
+                }
+            }
+            return result;
+        }
+};
+
+
 
 template<typename T>
-Matrix<T> dtanh(const Matrix<T>& M) {
-    Matrix<T> result(M.rows(), M.cols());
-    for (std::size_t i = 0; i < M.rows(); ++i) {
-        for (std::size_t j = 0; j < M.cols(); ++j) {
-            T t = std::tanh(M(i, j));
-            result(i, j) = T{1} - t * t;
+class Tanh : public Activation<T> {
+    public:
+        static Matrix<T> forward(const Matrix<T>& M) {
+            Matrix<T> result(M.rows(), M.cols());
+            for (std::size_t i = 0; i < M.rows(); ++i) {
+                for (std::size_t j = 0; j < M.cols(); ++j) {
+                    result(i, j) = std::tanh(M(i, j));
+                }
+            }
+            return result;
         }
-    }
-    return result;
-}
+
+        static Matrix<T> backward(const Matrix<T>& M) {
+            Matrix<T> result(M.rows(), M.cols());
+            for (std::size_t i = 0; i < M.rows(); ++i) {
+                for (std::size_t j = 0; j < M.cols(); ++j) {
+                    T t = std::tanh(M(i, j));
+                    result(i, j) = T{1} - t * t;
+                }
+            }
+            return result;
+        }
+};
