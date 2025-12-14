6c431872ef9c9bcf135a0b33e1dcc25503c7444a
fdallac <filippo.dallac@live.it>
2025-12-15 00:52:01 +0100

    Small fixes

--- tests/test_classification_neuralnet.cpp
+++ tests/test_classification_neuralnet.cpp
@@ -30,23 +30,24 @@ int main(int argc, char **argv) {
     Matrix<double> y_test = IOHelper<double>::read_csv("../data/preprocessed/breast_cancer/y_test.csv", ',', true);
 
     // Evaluate the neural network on test data
-    Matrix<double> predictions = nn.predict(X_test);
-    double test_loss = loss_function.forward(predictions, y_test);
-    std::cout << "====================" << std::endl;
+    Matrix<double> train_pred = nn.predict(X_train);
+    Matrix<double> test_pred = nn.predict(X_test);
     Accuracy<double> accuracy_metric;
     Precision<double> precision_metric;
     Recall<double> recall_metric;
-    double train_accuracy = accuracy_metric.eval_probs(nn.predict(X_train), y_train);
+
+    std::cout << "====================" << std::endl;
+    double train_accuracy = accuracy_metric.eval_probs(train_pred, y_train);
     std::cout << "Train Accuracy: " << train_accuracy << std::endl;
-    double test_accuracy = accuracy_metric.eval_probs(predictions, y_test);
+    double test_accuracy = accuracy_metric.eval_probs(test_pred, y_test);
     std::cout << "Test Accuracy: " << test_accuracy << std::endl;
-    double train_recall = recall_metric.eval_probs(nn.predict(X_train), y_train);
+    double train_recall = recall_metric.eval_probs(train_pred, y_train);
     std::cout << "Train Recall: " << train_recall << std::endl;
-    double test_recall = recall_metric.eval_probs(predictions, y_test);
+    double test_recall = recall_metric.eval_probs(test_pred, y_test);
     std::cout << "Test Recall: " << test_recall << std::endl;
-    double train_precision = precision_metric.eval_probs(nn.predict(X_train), y_train);
+    double train_precision = precision_metric.eval_probs(train_pred, y_train);
     std::cout << "Train Precision: " << train_precision << std::endl;
-    double test_precision = precision_metric.eval_probs(predictions, y_test);
+    double test_precision = precision_metric.eval_probs(test_pred, y_test);
     std::cout << "Test Precision: " << test_precision << std::endl;
 
     return 0;
